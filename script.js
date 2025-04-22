1dc542044700215f8abcb0a74ceb542b692bb635
malvi234 <malvi2@stumail.jccc.edu>
2025-04-21 21:34:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Author: Madina Alvi
+// Date: 2025-04-21
+
+// Variable to store full name
+let fullName = "Madina Alvi";
+console.log("Full Name:", fullName);
+
+// Variable to store desired annual salary
+let desiredSalary = 90000;
+console.log("Desired Salary:", desiredSalary);
+
+// Variable to store veteran status
+let isVeteran = false;
+console.log("Veteran Status:", isVeteran);
+
+// Array of three friend names
+let friendNames = ["Amanda", "Angela", "Kamea"];
+console.log("Friend Names:", friendNames);
+
+// Array of their desired salaries
+let friendSalaries = [95000, 90000, 85000];
+console.log("Friend Salaries:", friendSalaries);
+
+// Literal object for another friend
+let anotherFriend = {
+  firstName: "Mia",
+  lastName: "Garcia",
+  desiredSalary: 87000
+};
+console.log("Another Friend:", anotherFriend);
